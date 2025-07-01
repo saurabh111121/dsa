3f0f13305a0ca72ef21fdda1964d4674f211521e
kalasaur <kalasaur@amazon.com>
2025-07-01 20:59:38 +0530

    3330 Leetcode daily challenge

---
+++ src/com/leetcode/3330.find-the-original-typed-string-i.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=3330 lang=java
+ *
+ * [3330] Find the Original Typed String I
+ */
+
+// @lc code=start
+class Solution {
+    public int possibleStringCount(String word) {
+        int n = word.length();
+        int ans = 1;
+        for(int i=1;i<n;++i) {
+            if(word.charAt(i) == word.charAt(i-1)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
