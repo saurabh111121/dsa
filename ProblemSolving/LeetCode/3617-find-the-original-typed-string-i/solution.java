2eef2e6bb3264cc303622b298a843877eb00a53b
Saurabh kala <kalasaur@amazon.com>
2025-07-01 16:02:56 +0000

    feat(leetcode): sync accepted submissions Runtime - 1 ms (85.93%), Memory - 42 MB (99.70%)

---
+++ ProblemSolving/LeetCode/3617-find-the-original-typed-string-i/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int ans = 1;
+        int n = word.length();
+        for(int i=1;i<n;i++) {
+            if(word.charAt(i) == word.charAt(i-1)){
+                ans++;
+            }
+        }
+       return ans; 
+        
+    }
+}
+
+
