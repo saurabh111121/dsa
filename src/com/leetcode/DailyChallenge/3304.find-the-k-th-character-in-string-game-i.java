635d38d3ad5a7ee23549f00787f356a508e0ec5f
kalasaur <kalasaur@amazon.com>
2025-07-03 07:14:54 +0530

    3304. Leetcode Daily Challenge

---
+++ src/com/leetcode/DailyChallenge/3304.find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,22 @@
+package DailyChallenge;
+/*
+ * @lc app=leetcode id=3304 lang=java
+ *
+ * [3304] Find the K-th Character in String Game I
+ */
+
+// @lc code=start
+class Solution {
+    public char kthCharacter(int k) {
+        int ans = 0, t;
+        while(k != 1) {
+            t = 31 - Integer.numberOfLeadingZeros(k);
+            if((1 << t) == k) t--;
+            k = k - (1 << t);
+            ans++;
+        }
+        return (char) ('a' + ans);
+    }
+}
+// @lc code=end
+
