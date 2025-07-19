1ac138e2f80d803e9a0ad4a7d633985a54786c74
Saurabh kala <kalasaur@amazon.com>
2025-07-19 16:21:02 +0000

    [LeetCode Sync] Runtime - 40 ms (75.58%), Memory - 55.7 MB (30.31%)

---
+++ src/com/leetcode/1350-remove-sub-folders-from-the-filesystem/solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> res = new ArrayList<>();
+        String prev = "";
+        for (String f : folder) {
+            if (prev.isEmpty() || !f.startsWith(prev + "/")) {
+                res.add(f);
+                prev = f;
+            }
+        }
+        return res;
+    }
+}
+
