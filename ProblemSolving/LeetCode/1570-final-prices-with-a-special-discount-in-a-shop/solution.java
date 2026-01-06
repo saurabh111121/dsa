a478ea2ca319a19eba41d3da6a2def3602958f0a
Saurabh kala <kalasaur@amazon.com>
2026-01-06 01:18:24 +0000

    feat(leetcode): sync accepted submissions Runtime - 4 ms (53.20%), Memory - 45 MB (97.02%)

--- ProblemSolving/LeetCode/1570-final-prices-with-a-special-discount-in-a-shop/solution.java
+++ ProblemSolving/LeetCode/1570-final-prices-with-a-special-discount-in-a-shop/solution.java
@@ -1,22 +1,16 @@
 class Solution {
-    static int discount(int[] arr, int currentIndex, int n) {
-        int discount = 0;
-        for(int i = currentIndex; i < n ;i++) {
-           if(arr[i] <= arr[currentIndex] && i > currentIndex) {
-             discount = arr[i];
-             break;
-           }
-        }
-        return discount;
-        
-    }
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int[] arr = new int[n];
-        for(int i=0;i<n ;i++) {
-            int discount = discount(prices, i, n);
-            arr[i] = prices[i] - discount;
+        int[] ans = prices.clone();
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<n;i++) {
+            while(!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
+                int index = stack.pop();
+                ans[index] = prices[index] - prices[i];
+            }
+            stack.push(i);
         }
-        return arr;
+        return ans;
     }
 }
