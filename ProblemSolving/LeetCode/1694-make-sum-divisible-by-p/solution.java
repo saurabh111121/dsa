f94684bd31f70e11d864518bcd39323fb1bd0a29
Saurabh kala <kalasaur@amazon.com>
2026-01-14 20:29:09 +0000

    feat(leetcode): sync accepted submissions Runtime - 29 ms (39.49%), Memory - 98.2 MB (90.54%)

--- ProblemSolving/LeetCode/1694-make-sum-divisible-by-p/solution.java
+++ ProblemSolving/LeetCode/1694-make-sum-divisible-by-p/solution.java
@@ -1,28 +1,27 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
         int n = nums.length;
-        long total = 0;
-        for (int x : nums) total += x;
-        int target = (int)(total % p);
-        if (target == 0) return 0;
-        
-        Map<Integer, Integer> last = new HashMap<>();
-        last.put(0, -1); // prefix mod 0 at index -1
         int ans = n;
-        int cur = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            cur = (int)((cur + (long)nums[i]) % p);
-            int need = (cur - target) % p;
-            if (need < 0) need += p;
-            if (last.containsKey(need)) {
-                int len = i - last.get(need);
-                if (len < ans) ans = len;
-            }
-            // update the latest index for this prefix modulo
-            last.put(cur, i);
+        long total = 0;
+        for(int num:nums) {
+            total += num;
         }
-        
+        int rem = (int)(total%p);
+        if(rem == 0) return 0;
+        long prefix = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        for(int i =0;i<n;i++) {
+            prefix = (prefix + nums[i]) % p;
+            int x = (int)((prefix - rem + p) % p);
+            if(map.containsKey(x)){
+                ans = Math.min(ans, i-map.get(x));
+            }
+            map.put((int)prefix, i);
+        }
         return ans == n ? -1 : ans;
     }
 }
+
+
+
