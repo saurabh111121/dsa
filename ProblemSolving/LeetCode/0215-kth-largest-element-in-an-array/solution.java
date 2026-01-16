bcad7514c4cc8204b60d36062202a2b7ff43c881
Saurabh kala <kalasaur@amazon.com>
2026-01-16 22:07:51 +0000

    feat(leetcode): sync accepted submissions Runtime - 26 ms (92.24%), Memory - 70.3 MB (10.98%)

---
+++ ProblemSolving/LeetCode/0215-kth-largest-element-in-an-array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n - k];
+    }
+}
+/*
+
+
+
+*/
+
