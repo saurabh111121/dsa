b2249ab4fbdd101cc61ad99b1f6dcd38009a5358
kalasaur <kalasaur@amazon.com>
2026-02-22 12:55:26 +0530

    Binary Tree Insertion

--- DataStructures/Trees/TreeNode.java
+++ DataStructures/Trees/TreeNode.java
@@ -4,7 +4,13 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+
     TreeNode() {};
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+    
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
