e8eb97bc802a8a6d6c886683a455050a62ddefcf
kalasaur <kalasaur@amazon.com>
2026-02-22 12:16:40 +0530

    LeetCode 100

---
+++ DataStructures/Trees/SameTree.java
@@ -0,0 +1,16 @@
+package DataStructures.Trees;
+
+/*
+Leetcode Problem100 : https://leetcode.com/problems/same-tree/
+*/
+
+class SameTree extends TreeNode{
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null & q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
